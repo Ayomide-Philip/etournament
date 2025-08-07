3bee9ae0a3d768a1f8b7a7468c1b72964179598b
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-08-07 20:48:06 +0100

    upadating the admin pannel design

--- app/componet/adminPannel.js
+++ app/componet/adminPannel.js
@@ -5,7 +5,7 @@ import { IoIosFootball, IoMdNotifications } from "react-icons/io";
 import { MdExitToApp } from "react-icons/md";
 import { useState } from "react";
 import { TbTournament } from "react-icons/tb";
-import { Gauge, Settings, Trophy, User, Volleyball } from "lucide-react";
+import { Gauge, Settings, Table, Trophy, User, Volleyball } from "lucide-react";
 
 const darkMode = true;
 
@@ -16,102 +16,133 @@ export default function AdminPannel({ children }) {
 
   return (
     <div className={darkMode ? "dark" : ""}>
-      {/* Sidebar */}
       <div
-        className={`fixed inset-y-0 left-0 w-64 bg-cordes-dark shadow-xl z-50 dark:bg-gray-900 ${
+        className={`fixed inset-y-0 left-0 w-64 bg-cordes-dark shadow-xl z-50 dark:bg-gray-900 overflow-y-auto h-screen ${
           isSidebarOpen ? "block" : "hidden"
         } md:block md:w-64 transition-all duration-300`}
       >
-        <div className="flex items-center justify-between h-24 sm:h-20 bg-cordes-blue dark:bg-gray-800 px-4">
-          <div className="flex items-center space-x-3">
-            <div className="w-8 h-8 bg-white rounded-lg flex items-center justify-center">
-              <IoIosFootball />
-            </div>
-            <span className="text-white text-xl font-bold dark:text-gray-200">
-              ETournaments
-            </span>
-            <div className="flex items-center space-x-4 md:hidden">
-              <button
-                onClick={toggleSidebar}
-                className="text-gray-800 dark:text-white text-2xl focus:outline-none"
-                aria-label="Toggle Sidebar"
-              >
-                {isSidebarOpen ? "✕" : "☰"}
-              </button>
-            </div>
-          </div>
-        </div>
-
-        {/* Sidebar Navigation */}
-        <nav className="mt-8 px-4">
-          <div className="space-y-2">
-            <a
-              href="/"
-              className="flex items-center px-4 py-3 text-gray-300 hover:bg-gray-700 hover:text-white rounded-lg transition-colors group dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white"
-            >
-              <Gauge className="mr-3 text-gray-400 group-hover:text-white dark:text-gray-300" />
-              Dashboard
-            </a>
-            <a
-              href="#"
-              className="flex items-center px-4 py-3 text-gray-300 hover:bg-gray-700 hover:text-white rounded-lg transition-colors group dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white"
-            >
-              <User className="mr-3 text-gray-400 group-hover:text-white dark:text-gray-300" />
-              Users
-            </a>
-            <a
-              href="#"
-              className="flex items-center px-4 py-3 text-gray-300 hover:bg-gray-700 hover:text-white rounded-lg transition-colors group dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white"
-            >
-              <Trophy className="mr-3 text-gray-400 group-hover:text-white dark:text-gray-300" />
-              Leaderboard
-            </a>
-            <a
-              href="#"
-              className="flex items-center px-4 py-3 text-gray-300 hover:bg-gray-700 hover:text-white rounded-lg transition-colors group dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white"
-            >
-              <TbTournament className="mr-3 text-gray-400 group-hover:text-white dark:text-gray-300" />
-              Tournaments
-            </a>
-            <a
-              href="#"
-              className="flex items-center px-4 py-3 text-gray-300 hover:bg-gray-700 hover:text-white rounded-lg transition-colors group dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white"
-            >
-              <Volleyball className="mr-3 text-gray-400 group-hover:text-white dark:text-gray-300" />
-              Matches
-            </a>
-            <a
-              href="#"
-              className="flex items-center px-4 py-3 text-gray-300 hover:bg-gray-700 hover:text-white rounded-lg transition-colors group dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white"
-            >
-              <Settings className="mr-3 text-gray-400 group-hover:text-white dark:text-gray-300" />
-              Settings
-            </a>
-          </div>
-        </nav>
-
-        {/* Sidebar Footer */}
-        <div className="absolute bottom-4 left-4 right-4">
-          <div className="bg-gray-800 rounded-lg p-4 dark:bg-gray-700">
-            <div className="flex items-center justify-between space-x-3">
-              <div className="flex items-center space-x-3">
-                <img
-                  src="https://cdn-icons-png.flaticon.com/512/17003/17003310.png"
-                  alt="Admin"
-                  className="w-10 h-10 rounded-full"
-                />
-                <div>
-                  <p className="text-white text-sm font-medium dark:text-gray-200">
-                    John Admin
-                  </p>
-                  <p className="text-gray-400 text-xs dark:text-gray-400">
-                    Administrator
-                  </p>
-                </div>
+        <div className="flex flex-col justify-between h-full">
+          <div className="flex items-center justify-between h-24 p-8 sm:h-20 bg-cordes-blue dark:bg-gray-800 px-4">
+            <div className="flex items-center space-x-3">
+              <div className="w-8 h-8 bg-white rounded-lg flex items-center justify-center">
+                <IoIosFootball />
               </div>
-              <button className="bg-transparent text-white p-2 rounded-full hover:bg-gray-600">
-                <MdExitToApp size={24} />
-              </button>
+              <span className="text-white text-xl font-bold dark:text-gray-200">
+                ETournaments
+              </span>
+              <div className="flex items-center space-x-4 md:hidden">
+                <button
+                  onClick={toggleSidebar}
+                  className="text-gray-800 dark:text-white text-2xl focus:outline-none"
+                  aria-label="Toggle Sidebar"
+                >
+                  {isSidebarOpen ? "✕" : "☰"}
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <nav className="mt-4 px-4 flex-1">
+            <div className="space-y-2 pb-10 sm:pb-20">
+              <a
+                href="/"
+                className="flex items-center px-4 py-3 text-gray-300 hover:bg-gray-700 hover:text-white rounded-lg transition-colors group dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white"
+              >
+                <Gauge className="mr-3 text-gray-400 group-hover:text-white dark:text-gray-300" />
+                Dashboard
+              </a>
+              <a
+                href="#"
+                className="flex items-center px-4 py-3 text-gray-300 hover:bg-gray-700 hover:text-white rounded-lg transition-colors group dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white"
+              >
+                <User className="mr-3 text-gray-400 group-hover:text-white dark:text-gray-300" />
+                Users
+              </a>
+              <a
+                href="#"
+                className="flex items-center px-4 py-3 text-gray-300 hover:bg-gray-700 hover:text-white rounded-lg transition-colors group dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white"
+              >
+                <Trophy className="mr-3 text-gray-400 group-hover:text-white dark:text-gray-300" />
+                Leaderboard
+              </a>
+              <a
+                href="#"
+                className="flex items-center px-4 py-3 text-gray-300 hover:bg-gray-700 hover:text-white rounded-lg transition-colors group dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white"
+              >
+                <TbTournament className="mr-3 text-gray-400 group-hover:text-white dark:text-gray-300" />
+                Tournaments
+              </a>
+              <a
+                href="#"
+                className="flex items-center px-4 py-3 text-gray-300 hover:bg-gray-700 hover:text-white rounded-lg transition-colors group dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white"
+              >
+                <Table className="mr-3 text-gray-400 group-hover:text-white dark:text-gray-300" />
+                Table
+              </a>
+              <a
+                href="#"
+                className="flex items-center px-4 py-3 text-gray-300 hover:bg-gray-700 hover:text-white rounded-lg transition-colors group dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white"
+              >
+                <Volleyball className="mr-3 text-gray-400 group-hover:text-white dark:text-gray-300" />
+                Matches
+              </a>
+              <a
+                href="#"
+                className="flex items-center px-4 py-3 text-gray-300 hover:bg-gray-700 hover:text-white rounded-lg transition-colors group dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white"
+              >
+                <Settings className="mr-3 text-gray-400 group-hover:text-white dark:text-gray-300" />
+                Settings
+              </a>
+            </div>
+          </nav>
+
+          <div className="hidden md:block absolute bottom-4 left-4 right-4">
+            <div className="bg-gray-800 rounded-lg p-4 dark:bg-gray-700">
+              <div className="flex items-center justify-between space-x-3">
+                <div className="flex items-center space-x-3">
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/17003/17003310.png"
+                    alt="Admin"
+                    className="w-10 h-10 rounded-full"
+                  />
+                  <div>
+                    <p className="text-white text-sm font-medium dark:text-gray-200">
+                      John Admin
+                    </p>
+                    <p className="text-gray-400 text-xs dark:text-gray-400">
+                      Administrator
+                    </p>
+                  </div>
+                </div>
+                <button className="bg-transparent text-white p-2 rounded-full hover:bg-gray-600">
+                  <MdExitToApp size={24} />
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="md:hidden px-4 pb-4">
+            <div className="bg-gray-800 rounded-lg p-4 dark:bg-gray-700">
+              <div className="flex items-center justify-between space-x-3">
+                <div className="flex items-center space-x-3">
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/17003/17003310.png"
+                    alt="Admin"
+                    className="w-10 h-10 rounded-full"
+                  />
+                  <div>
+                    <p className="text-white text-sm font-medium dark:text-gray-200">
+                      John Admin
+                    </p>
+                    <p className="text-gray-400 text-xs dark:text-gray-400">
+                      Administrator
+                    </p>
+                  </div>
+                </div>
+                <button className="bg-transparent text-white p-2 rounded-full hover:bg-gray-600">
+                  <MdExitToApp size={24} />
+                </button>
+              </div>
             </div>
           </div>
         </div>
@@ -151,7 +182,6 @@ export default function AdminPannel({ children }) {
 
               <div className="hidden md:flex items-center space-x-4 mt-4 md:mt-0">
                 <div className="relative">
-                  <i className="fas fa-search absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 dark:text-gray-300"></i>
                   <input
                     type="text"
                     placeholder="Search..."
